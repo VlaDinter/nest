6db6f96027e452aabe656b1f9eceb83c346683c5
VlaDinter <vladyslavdinter@gmail.com>
2025-07-13 20:32:50 +0300

    Fix mail notifications

--- src/features/base/adapters/mail-notifications.ts
+++ src/features/base/adapters/mail-notifications.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 import { Logger } from './logger';
+import nodemailer from 'nodemailer';
 
 export class IMailNotifications {
   async sendEmail(
@@ -33,7 +34,7 @@ export class MailNotifications implements IMailNotifications {
   ): Promise<void> {
     await this.sendEmail(
       email,
-      'Email confirmation',
+      'AAA',
       `
       <h1>Thank for your registration</h1>
       <p>To finish registration please follow the link below:
@@ -49,7 +50,7 @@ export class MailNotifications implements IMailNotifications {
   ): Promise<void> {
     await this.sendEmail(
       email,
-      'Password recovery',
+      'BBB',
       `
       <h1>Password recovery</h1>
       <p>To finish password recovery please follow the link below:
@@ -59,19 +60,85 @@ export class MailNotifications implements IMailNotifications {
     );
   }
 
-  async sendEmail(
-    email: string,
-    title: string,
-    message: string,
-  ): Promise<void> {
-    try {
-      await this.mailerService.sendMail({
-        to: email,
-        html: message,
-        subject: title,
+  async sendEmail(email: string, title: string, message: string) {
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: 'smtp.gmail.com',
+      auth: {
+        user: process.env.EMAIL_FROM_USER,
+        pass: process.env.EMAIL_FROM_PASSWORD,
+      },
+      secure: true,
+    });
+
+    await new Promise((resolve, reject) => {
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log('Server is ready to take our messages');
+          resolve(success);
+        }
       });
-    } catch (error) {
-      this.logger.warn(error);
-    }
+    });
+
+    const mailData = {
+      from: {
+        name: 'Dimych',
+        address: '<dimychdeveloper@gmail.com>',
+      },
+      to: email,
+      subject: title,
+      html: message,
+    };
+
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          console.log(info);
+          resolve(info);
+        }
+      });
+    });
+
+    // const transport = nodemailer.createTransport({
+    //   service: 'gmail',
+    //   auth: {
+    //     user: process.env.EMAIL_FROM_USER,
+    //     pass: process.env.EMAIL_FROM_PASSWORD,
+    //   },
+    // });
+    //
+    // await transport.sendMail(
+    //   {
+    //     from: 'Dimych <dimychdeveloper@gmail.com>',
+    //     to: email,
+    //     subject: title,
+    //     html: message,
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   },
+    // );
   }
+
+  // async sendEmail(
+  //   email: string,
+  //   title: string,
+  //   message: string,
+  // ): Promise<void> {
+  //   try {
+  //     await this.mailerService.sendMail({
+  //       to: email,
+  //       html: message,
+  //       subject: title,
+  //     });
+  //   } catch (error) {
+  //     this.logger.warn(error);
+  //   }
+  // }
 }
